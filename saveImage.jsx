e2f180d3b48374c370f3400cc6d7f0a980300ddb
microjafar <tljafer@gmail.com>
2016-08-24 20:48:36 +0530

    Save Image

---
+++ saveImage.jsx
@@ -0,0 +1,16 @@
+﻿var doc = app.activeDocument; //active document
+var jpegQuality = 12; //Image Quality
+var folderPath = "C:/Users/micro/Downloads/"; //folder view its changable
+var imageName = "image.jpg"; //Image name to save its changable
+var filePath = folderPath + imageName;
+
+saveJPEG(doc, new File(filePath), jpegQuality);
+
+function saveJPEG(doc, path, qty ) {  
+     var saveOptions = new JPEGSaveOptions( );  
+     saveOptions.embedColorProfile = true;  
+     saveOptions.formatOptions = FormatOptions.STANDARDBASELINE;  
+     saveOptions.matte = MatteType.NONE;  
+     saveOptions.quality = qty;   
+     doc.saveAs( path, saveOptions, true );  
+}  
